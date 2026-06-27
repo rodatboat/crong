45f84c27e548a8aeea7e5ec6e249fecf48548ca2
rodatboat <ronaldo3055@gmail.com>
2026-06-27 12:05:27 -0500

    refactored job execution table and models

--- internal/entities/job_execution.go
+++ internal/entities/job_execution.go
@@ -25,11 +25,12 @@ type JobExecution struct {
 	DurationMs      int             `gorm:"column:duration_ms"`
 	Url             string          `gorm:"column:url"`
 
-	ResponseHeaders string    `gorm:"column:response_headers;type:text"`
-	ResponseBody    string    `gorm:"column:response_body;type:text"`
-	Error           string    `gorm:"column:error;type:text"`
-	ExecutedAt      time.Time `gorm:"column:executed_at;"`
-	PlannedFor      time.Time `gorm:"column:planned_for;"`
-	CreatedAt       time.Time `gorm:"column:created_at;default:now()"`
-	UpdatedAt       time.Time `gorm:"column:updated_at;default:now()"`
+	ResponseHeaders string `gorm:"column:response_headers;type:text"`
+	ResponseBody    string `gorm:"column:response_body;type:text"`
+	Error           string `gorm:"column:error;type:text"`
+
+	ExecutedAt time.Time `gorm:"column:executed_at;"`
+	PlannedFor time.Time `gorm:"column:planned_for;"`
+	CreatedAt  time.Time `gorm:"column:created_at;default:now()"`
+	UpdatedAt  time.Time `gorm:"column:updated_at;default:now()"`
 }
